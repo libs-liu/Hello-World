51ce4fefb8cd81d70c416e2a4c923c4593d42b4c
libs_liu <852064256@qq.com>
2016-07-03 14:11:19 +0800

    add source FOR VIM

---
+++ code/copy_and_paste/collection.js
@@ -0,0 +1,30 @@
+/***
+ * Excerpted from "Practical Vim",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/dnvim for more book information.
+***/collection
+articles
+ssomethingInTheWayomethingInTheWay
+拷贝一个词 不含空格
+拷贝collection 不含空格
+一个词一个词一个词一个词collection
+somethingInTheWaycollectioncsomethingInTheWayollection = getCollection(collection);collectioncollection
+psomethingInTheWaysomethin
+拷贝一个词 不含空格
+copy_and_paste/collection.js
+\<拷贝一个词\>
+
+拷贝一个词 不含空格
+gInTheWaycollectioncollectionrocess(, target);collectioncollection
+
+一一个词 不含空格
+gInTheWaycollectionco个词 不含空格
+gInTheWaycollectionco
+
+\<拷贝一个词\>
+
+
+
---
+++ test.c
@@ -0,0 +1,4 @@
+i am libs_liu
+you are who?
+haha
+comedkffjsdkf here
