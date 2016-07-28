13a58664c0d2fb00656d4b8d60e643ebc7ada3b4
libs_liu <852064256@qq.com>
2016-07-29 00:11:27 +0800

    create object

--- java_code/libs.java
+++ java_code/libs.java
@@ -1,5 +1,19 @@
 public class libs{
-public static void main(String[] args){
-System.out.println("hewloooo");
+	String name ="libs_rdd";
+	public static void main(String[] args){
+
+
+	System.out.println("hewloooo");
+	libs ll = new libs();
+	ll.bark();
+	ll.name = "libs_liu";
+	ll.bark();
+
+	
 }
+
+	public void bark(){
+		
+		System.out.println(name + ": Ruf Ruf!!");
+	}
 }
